54ae7d0246c7ab116b350b8a104eb413fe22634f
absalom <absalomsilwal93@gmail.com>
2025-02-14 17:28:53 +1100

    openai connection wip

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from models import db,Faq
 from flask_cors import CORS
 from sqlalchemy import text
-
+from openai import OpenAI
 
 load_dotenv()
 
@@ -13,6 +13,12 @@ app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+
+client = OpenAI(
+    api_key='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',
+    organization='org-3xUjBZ856pVdkeLYLikGH7ht',
+    project='proj_82tPsBj3lSEGtrrPZr1aAaTc',
+    ) 
 
 db.init_app(app)
 def seeder():
@@ -33,20 +39,28 @@ with app.app_context():
     seeder()
     
 
-@app.route('/ask',methods=['POST'])
+@app.route('/ask',methods=['GET'])
 def ask():
     try:
-        data = request.get_json()
-        question = data.get('question')
-        #question = "What products/services do you offer?"
+        #data = request.get_json()
+        #question = data.get('question')
+        question = "test?"
         faq_dict = {}
         faq = db.session.execute(text("SELECT * FROM faqs WHERE question ILIKE :question LIMIT 1"), {'question': f'%{question}%'}).fetchone()
         if faq:
             faq_dict = {
                 "answer": faq[2]
             }
+            #connect to openai
+            return jsonify(faq_dict)
+
+        response = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[{"role": "user", "content": "hey"}],
+            stream=True,
+        )
+        print(response)
         return jsonify(faq_dict)
-        #return faq
         #faq = Faq(question="How are you?",answer="I am good")
         #db.session.add(faq)
         #db.session.commit()
