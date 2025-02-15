40d473dc9de7464e861aaba4f59bfdce6ec08405
absalom <absalomsilwal93@gmail.com>
2025-02-15 15:32:59 +1100

    frontend mulitle qa shown on chat box

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,20 +6,26 @@ function App() {
   const [question,setQuestion] = useState('');
   const [answer,setAnswer]=useState([]);
   const [serverResponse,setServerResponse] = useState(false);
+  let [message,setMessage] = useState('');
   //const userQuestion = question?<div class="user-message"><p>{question}</p></div>:'';
   //const botAnswer = serverResponse?(answer?<div class="chatbot-message"><p>{answer.answer}</p></div>:'Sorry! Couldnot find the answer'):"";
-  let messages = '';
-  messages += question?`<div class="user-message"><p>${question}</p></div>`:''; 
-  messages +=  serverResponse?(answer?`<div class="chatbot-message"><p>${answer.answer}</p></div>`:'Sorry! Couldnot find the answer'):"";
+  message += serverResponse?'':(question?`<div class="user-message"><p>${question}</p></div>`:'');
+    //setMessage(messageEle);
   function onChangeQuestion(e){
-    setAnswer([]);
+    //setAnswer([]);
     setQuestion(e.target.value);
-    console.log(messages);
+    //serverResponse?setMessage(`${message}<div class="user-message"><p>${question}</p></div>`):setMessage(`<div class="user-message"><p>${question}</p></div>`);
+    //console.log(messages);
+  }
+
+  function onFocusQuestion(e){
+    e.preventDefault();
+    setServerResponse(false);
   }
 
   function callApi(e){
     e.preventDefault();
-    setServerResponse(false);
+    //setMessage(`${message}<div class="user-message"><p>${question}</p></div>`)
     fetch(`http://127.0.0.1:5000/ask`, {
       method: "POST", // HTTP method
       headers: {
@@ -31,9 +37,13 @@ function App() {
           throw new Error('Network response was not ok');
         }
         return response.json();
-    }).then((data)=>{
+    }).then((answer)=>{
       setServerResponse(true);
-      setAnswer(data);
+      //setAnswer(data);
+      const newMessage = `${message}<div class="chatbot-message"><p>${answer.answer}</p></div>`;
+      setMessage(newMessage);
+      setQuestion('');
+      document.getElementById('user-input').innerHTML='';
     })
   }
   return (
@@ -49,14 +59,14 @@ function App() {
                 <p>Ask me anything!</p>
             </div>
 
-            <div class="chat-box" id="chat-box" dangerouslySetInnerHTML={{ __html: messages }}>
+            <div class="chat-box" id="chat-box" dangerouslySetInnerHTML={{ __html: message }}>
                 {/* {userQuestion}
                 {userQuestion?botAnswer:''}  */}
                 
             </div>
 
             <div class="input-container">
-            <textarea type="text" id="user-input" onChange={onChangeQuestion} placeholder="Type your question..." ></textarea>
+            <textarea type="text" id="user-input" value={question} onFocus={onFocusQuestion} onChange={onChangeQuestion} placeholder="Type your question..." ></textarea>
                 <button>Send</button>
             </div>
         </div>
